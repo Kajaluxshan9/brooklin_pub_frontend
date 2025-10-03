66c3d252d9f207e6720832520a7738b51e093b30
Janujah <janujahsivarathinam@gmail.com>
2025-10-03 08:23:12 +0530

    Nav bar updated

---
+++ src/assets/components/menulist.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const menulist = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default menulist
---
+++ src/assets/pages/home.tsx
@@ -0,0 +1,17 @@
+import Nav from '../components/nav';
+import Item from '../components/items';
+import LandingPage from '../components/landingPage';
+import Footer from '../components/footer';
+
+const Home = () => {
+  return (
+    <div>
+      <Nav />
+      <LandingPage/>
+        <Item />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
