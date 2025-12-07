6325e31710e870d8bbf28aa02ee0baf22834b352
Kajaluxshan9 <tkajaluxshan@gmail.com>
2025-12-07 20:16:54 +0530

    final

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -548,7 +548,7 @@ const Home = () => {
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
               transition={{ duration: 0.5 }}
-                style={{
+              style={{
                 position: "fixed",
                 top: 0,
                 left: 0,
